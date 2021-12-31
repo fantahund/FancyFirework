cb65e3a8a65b110cae127dfce3a428bb98b64821
Brokkonaut <hannos17@gmx.de>
2021-12-31 05:40:44 +0100

    Limit voting to one vote per twelve hours per vote page

--- src/main/java/de/fanta/fancyfirework/listners/CubesideVoteListener.java
+++ src/main/java/de/fanta/fancyfirework/listners/CubesideVoteListener.java
@@ -7,6 +7,8 @@ import de.fanta.fancyfirework.utils.ChatUtil;
 import de.iani.playerUUIDCache.CachedPlayer;
 import de.iani.playerUUIDCache.PlayerUUIDCache;
 import de.iani.treasurechest.TreasureChest;
+import java.util.HashMap;
+import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
@@ -17,6 +19,9 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 public class CubesideVoteListener implements Listener {
     private final FancyFirework plugin = FancyFirework.getPlugin();
+
+    private static final long TWELVE_HOURS_IN_MILLIS = 1000L * 60 * 60 * 12;
+    private final HashMap<UUID, HashMap<String, Long>> votes = new HashMap<>();
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerVote(VotifierEvent e) {
@@ -36,13 +41,23 @@ public class CubesideVoteListener implements Listener {
         CachedPlayer cp = playerUUIDCache.getPlayerFromNameOrUUID(vote.getUsername().trim());
 
         if (cp != null && customfirework != null) {
+            HashMap<String, Long> lastVotes = votes.get(cp.getUniqueId());
+            if (lastVotes == null) {
+                lastVotes = new HashMap<>();
+                votes.put(cp.getUniqueId(), lastVotes);
+            }
+            Long lastTime = lastVotes.get(vote.getServiceName());
+            if (lastTime != null & lastTime + TWELVE_HOURS_IN_MILLIS > System.currentTimeMillis()) {
+                return;
+            }
+            lastVotes.put(vote.getServiceName(), System.currentTimeMillis());
 
             ItemStack displayItem = new ItemStack(Material.FIREWORK_ROCKET, 1);
             ItemMeta displayItemMeta = displayItem.getItemMeta();
             displayItemMeta.setDisplayName(ChatUtil.GREEN + "FancyFirework");
             displayItem.setItemMeta(displayItemMeta);
 
-            treasureChest.addItem(cp.getName(), displayItem, new ItemStack[]{customfirework.clone()}, 0);
+            treasureChest.addItem(cp.getName(), displayItem, new ItemStack[] { customfirework.clone() }, 0);
             Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "tell " + cp.getName() + " " + FancyFirework.PREFIX + ChatUtil.GREEN + " Du hast durchs Voten " + customfirework.getItemMeta().getDisplayName() + ChatUtil.GREEN + " erhalten und kannst diese in der Schatztruhe am Spawn abholen!");
         }
     }
--- src/main/java/de/fanta/fancyfirework/listners/VoteListener.java
+++ src/main/java/de/fanta/fancyfirework/listners/VoteListener.java
@@ -4,6 +4,8 @@ import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.model.VotifierEvent;
 import de.fanta.fancyfirework.FancyFirework;
 import de.fanta.fancyfirework.utils.ChatUtil;
+import java.util.HashMap;
+import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,6 +16,9 @@ import org.bukkit.inventory.ItemStack;
 public class VoteListener implements Listener {
     private final FancyFirework plugin = FancyFirework.getPlugin();
 
+    private static final long TWELVE_HOURS_IN_MILLIS = 1000L * 60 * 60 * 12;
+    private final HashMap<UUID, HashMap<String, Long>> votes = new HashMap<>();
+
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerVote(VotifierEvent e) {
         Vote vote = e.getVote();
@@ -23,6 +28,17 @@ public class VoteListener implements Listener {
             return;
         }
         if (p != null) {
+            HashMap<String, Long> lastVotes = votes.get(p.getUniqueId());
+            if (lastVotes == null) {
+                lastVotes = new HashMap<>();
+                votes.put(p.getUniqueId(), lastVotes);
+            }
+            Long lastTime = lastVotes.get(vote.getServiceName());
+            if (lastTime != null & lastTime + TWELVE_HOURS_IN_MILLIS > System.currentTimeMillis()) {
+                return;
+            }
+            lastVotes.put(vote.getServiceName(), System.currentTimeMillis());
+
             if (p.getInventory().firstEmpty() == -1) {
                 ChatUtil.sendErrorMessage(p, "Your inventory is full " + customfirework.getItemMeta().getDisplayName() + ChatUtil.GREEN + " could not be added to your inventory.");
             } else {
