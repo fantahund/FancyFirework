0f1a8a76c28d8cb56a82581cada9e8da9368d846
fanta <tobiasmuellner@outlook.com>
2023-01-01 16:15:07 +0100

    Give firework to other Player

--- src/main/java/de/fanta/fancyfirework/commands/FireWorkGiveCommand.java
+++ src/main/java/de/fanta/fancyfirework/commands/FireWorkGiveCommand.java
@@ -5,6 +5,7 @@ import de.fanta.fancyfirework.fireworks.AbstractFireWork;
 import de.fanta.fancyfirework.utils.ChatUtil;
 import de.iani.cubesideutils.bukkit.commands.SubCommand;
 import de.iani.cubesideutils.commands.ArgsParser;
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -30,15 +31,16 @@ public class FireWorkGiveCommand extends SubCommand {
             return true;
         }
 
-        if (args.remaining() != 1 && args.remaining() != 2) {
+        /*if (args.remaining() != 1 && args.remaining() != 2) {
             ChatUtil.sendWarningMessage(p, "/fancyfirework give [key] <amount>");
             return true;
-        }
+        }*/
 
+        Player givePlayer = p;
         if (args.hasNext()) {
             String key = args.getNext();
             AbstractFireWork fireWork = plugin.getRegistry().get(NamespacedKey.fromString(key));
-            int amount = 1;
+            int amount;
             if (args.hasNext()) {
                 int next = args.getNext(1);
                 if (next < 1 || next > 64) {
@@ -46,20 +48,43 @@ public class FireWorkGiveCommand extends SubCommand {
                     return true;
                 }
                 amount = next;
+
+                if (args.hasNext()) {
+                    if (!p.hasPermission(FancyFirework.ADMIN_PERMISSION)) {
+                        ChatUtil.sendErrorMessage(p, "You do not have the permission to give fireworks to other players.");
+                        return true;
+                    }
+                    String giveName = args.getNext();
+                    givePlayer = Bukkit.getPlayer(giveName);
+
+                    if (givePlayer == null) {
+                        ChatUtil.sendErrorMessage(p, "Player " + giveName + " is not online.");
+                        return true;
+                    }
+                }
+            } else {
+                ChatUtil.sendWarningMessage(p, "/fancyfirework give [key] <amount> <PlayerName>");
+                return true;
             }
             if (fireWork != null) {
-                Inventory inv = p.getInventory();
+                Inventory inv = givePlayer.getInventory();
                 ItemStack itemStack = fireWork.getItemStack();
                 itemStack.setAmount(amount);
                 if (inv.firstEmpty() == -1) {
                     ChatUtil.sendErrorMessage(p, "Item could not be added. Your inventory is full!");
                 } else {
-                    p.getInventory().addItem(itemStack);
-                    ChatUtil.sendNormalMessage(p, amount + "x " + itemStack.getItemMeta().getDisplayName() + ChatUtil.GREEN + " has been added to your inventory");
+                    givePlayer.getInventory().addItem(itemStack);
+                    ChatUtil.sendNormalMessage(givePlayer, amount + "x " + itemStack.getItemMeta().getDisplayName() + ChatUtil.GREEN + " has been added to your inventory");
+                    if (!p.getUniqueId().equals(givePlayer.getUniqueId())) {
+                        ChatUtil.sendNormalMessage(p, amount + "x " + itemStack.getItemMeta().getDisplayName() + ChatUtil.GREEN + " has been added to " + givePlayer.getName() + "'s inventory");
+                    }
                 }
             } else {
                 ChatUtil.sendErrorMessage(p, "Firework " + key + " not available.");
             }
+        } else {
+            ChatUtil.sendWarningMessage(p, "/fancyfirework give [key] <amount> <PlayerName>");
+            return true;
         }
         return true;
     }
@@ -72,6 +97,13 @@ public class FireWorkGiveCommand extends SubCommand {
                 ArrayList<String> li = new ArrayList<>();
                 plugin.getRegistry().getKeys().forEach((x) -> li.add(x.toString()));
                 return li;
+            }
+            if (sender.hasPermission(FancyFirework.ADMIN_PERMISSION)) {
+                if (i == 3) {
+                    ArrayList<String> li = new ArrayList<>();
+                    Bukkit.getOnlinePlayers().forEach((player) -> li.add(player.getName()));
+                    return li;
+                }
             }
             return new ArrayList<>();
         }
